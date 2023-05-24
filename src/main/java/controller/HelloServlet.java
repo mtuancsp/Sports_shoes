f420b59478808c8ed3c60b47d2ef49c6b087300a
mtuancsp <minhtuan267@gmail.com>
2023-05-24 16:12:24 +0700

    ;

--- src/main/java/controller/HelloServlet.java
+++ src/main/java/controller/HelloServlet.java
@@ -9,13 +9,8 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
     }
 
 }

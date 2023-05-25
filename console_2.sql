695b1e5293761a52b837cdfef613b7a361bffb82
minhcong213274 <gathidungru@gmail.com>
2023-05-25 08:17:48 +0700

    ;

--- console_2.sql
+++ console_2.sql
@@ -1,8 +1,8 @@
-USE mydb;
-INSERT INTO users (username, password, phone, email, role, fullName, birthdate, avatar_path)
+
+INSERT INTO users (username, password, phone, email, role, full_name, birthday, avatar_path)
 VALUES ('john_doe', 'password123', '1234567890', 'john.doe@example.com', 'customer', 'John Doe', '1990-01-01', NULL);
 
-INSERT INTO users (username, password, phone, email, role, fullName, birthdate, avatar_path)
+INSERT INTO users (username, password, phone, email, role, full_name, birthday, avatar_path)
 VALUES ('jane_doe', 'password123', '0987654321', 'jane.doe@example.com', 'seller', 'Jane Doe', '1995-01-01', NULL);
 
 INSERT INTO categories (category_id, category_name)
@@ -15,19 +15,19 @@ INSERT INTO categories (category_id, category_name)
 VALUES (3, 'Home and Garden');
 
 
-INSERT INTO products (product_id, product_name, price, description, user_id, category_id, quantityInStock)
+INSERT INTO products (product_id, product_name, price, description, user_id, category_id, quantity_in_stock)
 VALUES (1, 'iPhone 12', 999, 'A high-end smartphone from Apple', 2, 1, 10);
 
-INSERT INTO products (product_id, product_name, price, description, user_id, category_id, quantityInStock)
+INSERT INTO products (product_id, product_name, price, description, user_id, category_id, quantity_in_stock)
 VALUES (2, 'Samsung Galaxy S21', 899, 'A high-end smartphone from Samsung', 2, 1, 15);
 
-INSERT INTO products (product_id, product_name, price, description, user_id, category_id, quantityInStock)
+INSERT INTO products (product_id, product_name, price, description, user_id, category_id, quantity_in_stock)
 VALUES (3, 'Men\'s T-shirt', 25, 'A comfortable and stylish T-shirt for men', 2, 2, 20);
 
-INSERT INTO products (product_id, product_name, price, description, user_id, category_id, quantityInStock)
+INSERT INTO products (product_id, product_name, price, description, user_id, category_id, quantity_in_stock)
 VALUES (4, 'Women\'s Dress', 50, 'A beautiful and elegant dress for women', 2, 2, 5);
 
-INSERT INTO products (product_id, product_name, price, description, user_id, category_id, quantityInStock)
+INSERT INTO products (product_id, product_name, price, description, user_id, category_id, quantity_in_stock)
 VALUES (5, 'Kitchen Knife Set', 99, 'A high-quality set of kitchen knives', 2, 3, 8);
 
 INSERT INTO addresses (user_id, details, ward, district, city)
@@ -42,13 +42,13 @@ VALUES (4, 'Great phone!', 1);
 INSERT INTO reviews (rating, review, product_id)
 VALUES (5, 'Love this dress!', 4);
 
-INSERT INTO productimages (product_id, image_path)
+INSERT INTO product_images (product_id, image_path)
 VALUES (1, 'images/iphone12.jpg');
 
-INSERT INTO productimages (product_id, image_path)
+INSERT INTO product_images (product_id, image_path)
 VALUES (2, 'images/samsungs21.jpg');
 
-INSERT INTO productimages (product_id, image_path)
+INSERT INTO product_images (product_id, image_path)
 VALUES (4, 'images/womensdress.jpg');
 
 INSERT INTO payments (payment_method, order_id, amount)
@@ -65,13 +65,13 @@ INSERT INTO orders (order_date, status, comment, user_id, payment_id)
 VALUES ('2022-01-03 10:00:00', 'Pending', 'Processing your order', 2, 2);
 
 
-INSERT INTO orderdetails (order_id, product_id, quantity)
+INSERT INTO order_details (order_id, product_id, quantity)
 VALUES (1, 1, 1);
 
-INSERT INTO orderdetails (order_id, product_id, quantity)
+INSERT INTO order_details (order_id, product_id, quantity)
 VALUES (1, 2, 1);
 
-INSERT INTO orderdetails (order_id, product_id, quantity)
+INSERT INTO order_details (order_id, product_id, quantity)
 VALUES (2, 4, 1);
 
 
--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -9,7 +9,25 @@ import java.util.List;
 
 public class UserDAO {
     private static final String SELECT_ALL_USERS = "select * from users";
-    private static final String INSERT_USERS_SQL = "INSERT INTO users (username,password,phone, email,full_name,birthday) VALUES (?, ?, ?, ?, ?, ?);";
+    private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
+    private static final String FIND_USER_BY_PHONE = "select user_id,full_name,email,phone,birthday from users where phone =?";
+    private static final String SORT_BY_NAME = "select user_id,full_name,email,phone,birthday from users order by username;";
+    private static final String SORT_BY_CREATE_AT = "select user_id,full_name,email,phone,birthday from users order by created_at;";
+    private static final String INSERT_USERS_SQL = "insert into users(username, password, phone, email, full_name, birthday) values  (?,?,?,?,?,?);";
+    public void insertUser(Users user) throws SQLException {
+        System.out.println(INSERT_USERS_SQL);
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getPhone());
+            preparedStatement.setString(4, user.getEmail());
+            preparedStatement.setString(5, user.getFull_name());
+            preparedStatement.setDate(6, (java.sql.Date) user.getBirthday());
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+        }
+    }
     public List<Users> selectAllUsers(){
         List<Users> users = new ArrayList<>();
         try(Connection connection = DatabaseConnector.getConnection();
@@ -27,10 +45,57 @@ public class UserDAO {
                 Date birthday = resultSet.getDate("birthday");
                 String avatar_path = resultSet.getString("avatar_path");
                 Timestamp created_at = resultSet.getTimestamp("created_at");
-               users.add(new Users(user_id,username,password,phone,email,role,full_name,birthday,avatar_path,created_at));
+                String status = resultSet.getString("status");
+               users.add(new Users(user_id,username,password,phone,email,role,full_name,birthday,avatar_path,created_at,status));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }return users;
     }
+    public boolean deleteUser(int id) throws SQLException{
+        boolean rowDeleted;
+        try( Connection connection = DatabaseConnector.getConnection();
+        PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL)) {
+            statement.setInt(1,id);
+            System.out.println(statement);
+            rowDeleted = statement.executeUpdate()>0;
+        }return rowDeleted;
+    }
+    public void sortByName() throws SQLException{
+        try( Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement statement = connection.prepareStatement(SORT_BY_NAME)) {
+            System.out.println(statement);
+            statement.executeUpdate();
+        }
+    }
+    public void sortByCreateAt() throws SQLException{
+        try( Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement statement = connection.prepareStatement(SORT_BY_CREATE_AT)) {
+            statement.executeUpdate();
+        }
+    }
+    public Users findUserByPhone(){
+        Users user;
+        try( Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement statement = connection.prepareStatement(FIND_USER_BY_PHONE)) {
+            System.out.println(statement);
+            ResultSet resultSet = statement.executeQuery();
+                int user_id = resultSet.getInt("user_id");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                String phone = resultSet.getString("phone");
+                String email = resultSet.getString("email");
+                String role = resultSet.getString("role");
+                String full_name = resultSet.getString("full_name");
+                Date birthday = resultSet.getDate("birthday");
+                String avatar_path = resultSet.getString("avatar_path");
+                Timestamp created_at = resultSet.getTimestamp("created_at");
+                String status = resultSet.getString("status");
+                user = new Users(user_id,username,password,phone,email,role,full_name,birthday,avatar_path,created_at,status);
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }return user;
+    }
+
 }

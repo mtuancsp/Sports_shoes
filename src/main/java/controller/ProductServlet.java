a89bf6def58e2f32fe3202b6cfc8943abf25b6ca
FiveWing96 <117134067+FiveWing96@users.noreply.github.com>
2023-05-25 15:08:58 +0700

    data

--- src/main/java/controller/ProductServlet.java
+++ src/main/java/controller/ProductServlet.java
@@ -1,12 +1,25 @@
 package controller;
 
+import dao.ProductDao;
+import model.Products;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "ProductServlet", value = "/ProductServlet")
 public class ProductServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private ProductDao productDao;
+    public void init(){
+        productDao = new ProductDao();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -18,4 +31,19 @@ public class ProductServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
+
+    private void listProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException, SQLException{
+        List<Products> listProduct = productDao.selectAllProduct();
+        request.setAttribute("products",listProduct);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("");
+        requestDispatcher.forward(request,response);
+    }
+    private void showEditFrom(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException,SQLException{
+      String product_name = request.getParameter("product_name");
+
+
+
+    }
+
+
 }

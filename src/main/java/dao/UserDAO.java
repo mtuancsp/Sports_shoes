5a854d8decc1118dc9832199bcb07c9ab9975f25
FiveWing96 <117134067+FiveWing96@users.noreply.github.com>
2023-05-26 21:09:27 +0700

    tonight 21h:09

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -14,6 +14,8 @@ public class UserDAO {
     private static final String SORT_BY_NAME = "select user_id,full_name,email,phone,birthday from users order by username;";
     private static final String SORT_BY_CREATE_AT = "select user_id,full_name,email,phone,birthday from users order by created_at;";
     private static final String INSERT_USERS_SQL = "insert into users(username,password,phone,email,full_name) values  (?,?,?,?,?);";
+    private static final String SELECT_USER_BY_ID ="select username,password,phone,email,full_name from users where user_name = ?";
+    private static final String UPDATE_USERS_SQL = "update users set username = ?,password =?,phone = ?,email = ?,full_name =? where user_name = ?;";
     public void insertUser(Users user) throws SQLException {
         System.out.println(INSERT_USERS_SQL);
         try (Connection connection = DatabaseConnector.getConnection();
@@ -51,14 +53,13 @@ public class UserDAO {
             throw new RuntimeException(e);
         }return users;
     }
-    public boolean deleteUser(int id) throws SQLException{
+    public boolean deleteUser(int id) throws SQLException {
         boolean rowDeleted;
-        try( Connection connection = DatabaseConnector.getConnection();
-        PreparedStatement statement = connection.prepareStatement(DELETE_USERS_BY_ID)) {
-            statement.setInt(1,id);
-            System.out.println(statement);
-            rowDeleted = statement.executeUpdate()>0;
-        }return rowDeleted;
+        try (Connection connection = DatabaseConnector.getConnection(); PreparedStatement statement = connection.prepareStatement(DELETE_USERS_BY_ID);) {
+            statement.setInt(1, id);
+            rowDeleted = statement.executeUpdate() > 0;
+        }
+        return rowDeleted;
     }
     public void sortByName() throws SQLException{
         try( Connection connection = DatabaseConnector.getConnection();
@@ -95,5 +96,39 @@ public class UserDAO {
         }catch (SQLException e) {
             throw new RuntimeException(e);
         }return user;
+    }
+    public boolean updateUser(Users user) throws SQLException {
+        boolean rowUpdated;
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(UPDATE_USERS_SQL);
+        ){
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getPhone());
+            statement.setString(4, user.getEmail());
+            statement.setString(5, user.getFull_name());
+            rowUpdated = statement.executeUpdate() > 0;
+        }
+        return rowUpdated;
+    }
+    public Users selectUser(String name) {
+        Users user = null;
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);) {
+            preparedStatement.setString(1, name);
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String phone = rs.getString("phone");
+                String email = rs.getString("email");
+                String full_name = rs.getString("full_name");
+                user = new Users(username,password,phone,email,full_name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 }
---
+++ src/main/webapp/listUser.jsp
@@ -0,0 +1,31 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ngoct
+  Date: 5/26/2023
+  Time: 8:51 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>User Management Application</title>
+</head>
+<body>
+<center>
+  <h1>User Management</h1>
+  <h2>
+    <a href="/users?action=create">Add New User</a>
+  </h2>
+</center>
+<div align="center">
+  <table border="1" cellpadding="5">
+    <caption><h2>List of Users</h2></caption>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Country</th>
+      <th>Actions</th>
+    </tr>
+</body>
+</html>
